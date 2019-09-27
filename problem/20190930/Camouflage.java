78712eeba9fba9b92574d9a2141bbaa518482a12
SongHae <thdgo456@gmail.com>
2019-09-27 16:41:32 +0900

    make a problem for 2019.09.30

---
+++ problem/20190930/Camouflage.java
@@ -0,0 +1,26 @@
+public class Camouflage {
+
+	public static void main(String[] args) {
+		solution(new String[][] {{"yellow_hat", "headgear"}, 
+								{"blue_sunglasses", "eyewear"}, 
+								{"green_turban", "headgear"}});
+		
+		solution(new String[][] {{"crow_mask", "face"}, 
+								{"blue_sunglasses", "face"}, 
+								{"smoky_makeup", "face"}});
+
+	}
+	
+	
+    public static int solution(String[][] clothes) {
+    	int answer= 0;
+    	
+        return answer= -1;
+    }
+}
+
+
+/*
+ * 문제 : https://programmers.co.kr/learn/courses/30/lessons/42578
+ * 위장 level 2
+ * */
---
+++ problem/20190930/MockTest.java
@@ -0,0 +1,18 @@
+public class MockTest {
+
+	public static void main(String[] args) {
+		solution(new int[] {1,2,3,4,5});
+		solution(new int[] {1,3,2,4,2});
+
+	}
+
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+/*
+문제 : https://programmers.co.kr/learn/courses/30/lessons/42840?language=java
+모의고사 lv1
+*/
