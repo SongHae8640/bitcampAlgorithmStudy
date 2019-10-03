6ad42fabbaf0f5fbccb6f79787a7d82b940a741d
JuheeLee <kkakiyogu@gmail.com>
2019-10-03 19:35:28 +0900

    day3 algorism

---
+++ problem/20191007/GreatestNumber.java
@@ -0,0 +1,10 @@
+
+public class GreatestNumber {
+	public String main(int[] numbers) {
+        String answer = "";
+        return answer;
+    }
+}
+/* Lv2
+ https://programmers.co.kr/learn/courses/30/lessons/42746
+ */
---
+++ problem/20191007/Marathon.java
@@ -0,0 +1,15 @@
+
+public class Marathon {
+	
+	public static String main(String[] args) {
+		String answer = "";
+        return answer;
+
+	}
+	
+}
+
+
+/*Lv 1
+ https://programmers.co.kr/learn/courses/30/lessons/42576
+ */
