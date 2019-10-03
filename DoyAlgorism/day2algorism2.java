451719d49a2e137e30f14d48be1dd8d9cef69bab
DoYoung <dlaehxuf@gmail.com>
2019-10-03 18:38:32 +0900

    day2

---
+++ DoyAlgorism/day2algorism2.java
@@ -0,0 +1,31 @@
+package DoyAlgorism;
+
+public class day2algorism2 {
+
+	public static int main(String[] args) {
+		// TODO Auto-generated method stub
+		int answer = 0;
+		int div = 1000000007;
+		
+		int n=10;
+		
+		int a=1;
+		int b=1;
+		int total=0;
+		for (int i = 0; i < n-1; i++) {
+			total=(a+b)%div;
+			a=b;
+			b=total;
+		}
+		System.out.println(total);
+		
+	    return answer;
+
+	}
+
+}
+
+/*https://programmers.co.kr/learn/courses/30/lessons/12900
+ *타일링 lv3 
+ * 
+ */
