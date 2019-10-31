caf151d678cfd5e674a25b5cd33d5ea64b462ec8
Greeense <grandse@naver.com>
2019-10-31 21:49:47 +0900

    2019-11-04 lv4 problem 1

---
+++ 20191104_problem/Muji_Live.java
@@ -0,0 +1,17 @@
+package javapro;
+
+public class Muji_Live {
+	public static void main(String[] args){
+		int[] food_times = {3,1,2};
+		long k = 5;
+		//return 1
+		//https://programmers.co.kr/learn/courses/30/lessons/42891
+	}
+	
+	public class Solution {
+	    public int solution(int[] food_times, long k) {
+	        int answer = 0;
+	        return answer;
+	    }
+	}
+}
