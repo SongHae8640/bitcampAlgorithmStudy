fbb3566a58b1412d2b6e748d1aa6dbbeb5d63866
SongHae <thdgo456@gmail.com>
2019-11-14 12:26:31 +0900

    solve 20191114 problem

--- problem/20191114/No1100WhiteBlank.java
+++ problem/20191114/No1100WhiteBlank.java
@@ -1,15 +1,35 @@
-package backjoon;
+import java.util.Scanner;
 
 public class No1100WhiteBlank {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int pawnCountInWhite =0;
+		
+		for(int rowNum = 0; rowNum < 8 ; rowNum++) {
+			String row = sc.next();
+			char[] colInRow = row.toCharArray();
+			
+			for (int colNum = 0; colNum < colInRow.length; colNum++) {
+				if(( (rowNum+colNum)%2==0) && colInRow[colNum] == 'F') {
+					pawnCountInWhite++;
+				}
+			}
 			
 			
 		}
-	
+		
+		System.out.println(pawnCountInWhite);
+		
+
+	}
+
 }
 
 /*
- * 
- *  https://www.acmicpc.net/problem/1100
+ *  문제 출처 : https://www.acmicpc.net/problem/1100
+ *  
+ *  메모리:14192KB	
+ *  시간 : 100ms
  */
--- problem/20191114/No1668ThophyDisplay.java
+++ problem/20191114/No1668ThophyDisplay.java
@@ -1,13 +1,52 @@
-package backjoon;
+
+
+import java.util.Scanner;
 
 public class No1668ThophyDisplay {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		
+		int[] trophys = new int[n];
+		int maxHeight;
+		int visibleTrophyNum; 
+		
+		for (int i = 0; i < n; i++) {
+			trophys[i] = sc.nextInt();
+		}
+		
+		
+		//왼쪽에서 보기
+		maxHeight = 0;
+		visibleTrophyNum = 0;
+		for (int i = 0; i < trophys.length; i++) {
+			if(trophys[i] > maxHeight) {
+				visibleTrophyNum++;
+				maxHeight = trophys[i];
+			}
+		}
+		System.out.println(visibleTrophyNum);
+		
+		
+		//오른쪽에서 보기
+		maxHeight = 0;
+		visibleTrophyNum = 0;
+		for (int i = trophys.length-1; i >=0 ; i--) {
+			if(trophys[i] > maxHeight) {
+				visibleTrophyNum++;
+				maxHeight = trophys[i];
+			}
+		}
+		System.out.println(visibleTrophyNum);
 	}
 
 }
 
 /*
- * https://www.acmicpc.net/problem/1668
- */
+ * 출처 : https://www.acmicpc.net/problem/1668
+ * 
+ * 메모리 : 14292KB
+ * 시간 : 108ms
+ * */
