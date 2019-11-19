ee33fa17288ed87151314bb120fe3a9ba8d6dcad
Greeense <grandse@naver.com>
2019-11-19 16:17:10 +0900

    20191121 problem

---
+++ 20191121/bj11650.java
@@ -0,0 +1,5 @@
+package javapro;
+
+public class bj11650 {
+//https://www.acmicpc.net/problem/11650
+}
---
+++ 20191121/bj1431.java
@@ -0,0 +1,5 @@
+package javapro;
+
+public class bj1431 {
+//https://www.acmicpc.net/problem/1431
+}
